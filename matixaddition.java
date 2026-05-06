e7dc545b5f333c7d343fc26735489f61f94b3a68
Saimon <saimongiri401@gmail.com>
2026-05-06 19:16:14 +0545

    Two dimension matrix addition

---
+++ matixaddition.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class matixaddition {
+    public static void main(String[] args) {
+        int i,j;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter order of matrix: ");
+        int n = s.nextInt();
+        int[][] matA = new int[n][n];
+        int[][] matB = new int[n][n];
+        int[][] result = new int[n][n];
+        System.out.println("Read first matrix");
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                System.out.println("matA["+i+"]["+j+"]:");
+                matA[i][j] = s.nextInt();
+            }
+        }
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                System.out.println("matB["+i+"]["+j+"]:");
+                matB[i][j] = s.nextInt();
+            }
+        }
+        System.out.println("The addition of two matrix: ");
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                result[i][j]= matA[i][j]+matB[i][j];
+                System.out.println("Result["+i+"]["+j+"]: "+result[i][j]);
+            }
+        }
+    }
+    
+}
